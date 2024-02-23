5edd85ed4bc6aa80cc8176779c42e08fab98b30d
552020 <stefanolombardo@posteo.de>
2024-02-23 22:34:29 +0100

    reshuffle

--- README.md
+++ README.md
@@ -1,12 +1,69 @@
 # CPP_Module_01
 
-## ex05
+## ex00 - BraiiiiiiinnnzzzZ
 
-## ex06
+## ex01 - Moar brainz!
 
-- switch statement
+## 02 - HI THIS IS BRAIN
 
-- enum
+## 03 - Unnecessary violence
+
+## 04 - Sed is for losers
+
+- _File manipulatio_
+
+## Headers
+
+fstream: ifstram, ofstream
+iostream: cerr, cout
+sstream: ostringstream
+string: string
+
+### Note 1 `std::ifstream input_file(path.c_str());`
+
+```c++
+	std::ifstream input_file(path.c_str());
+	if (!input_file.is_open())
+	{
+		std::cerr << "Could not open the file - '" << path << "'" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+```
+
+The _c_str()_ function is a member function of the std::string class in C++. It returns a pointer to an array that contains a null-terminated sequence of characters (i.e., a C-string) representing the current value of the std::string object.
+
+In readFileIntoString c_str() is called on the path variable, which is of type std::string, to convert it to a C-style string since In C++98, the constructor of std::ifstream (input file stream) does not accept a std::string directly. Instead, it expects a `const char*` type, which is the traditional C-style string.
+
+### Note 2 `buffer << input_file.rdbuf();`
+
+```c++
+	std::ostringstream buffer;
+	buffer << input_file.rdbuf();
+```
+
+Other possible syntaxes:
+
+- `std::ostringstream buffer(input_file.rdbuf());`
+- `std::ostringstream buffer << input_file.rdbuf();`
+
+Or without std::ostringstram
+
+```c++
+std::string buffer;
+std::string line;
+while (std::getline(input_file, line)) {
+	buffer += line + '\n'; // Repeatedly append to the string
+}
+```
+
+## ex05 - Harl 2.0
+
+- _Pointers to members_
+
+## ex06 - Harl filter
+
+- _Switch statement_
+- Enum
 
 ```c++
 // EnumLevel is a new type
--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,25 +1,19 @@
-#include <fstream>	// ifstream, ofstream
-#include <iostream> // cerr, cout
-#include <sstream>	// ostringstream
-#include <string>	// getline, string
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
 
 std::string readFileIntoString(const std::string &path)
 {
-	// Note 1
 	std::ifstream input_file(path.c_str());
-	// Note 2
 	if (!input_file.is_open())
 	{
 		std::cerr << "Could not open the file - '" << path << "'" << std::endl;
 		exit(EXIT_FAILURE);
 	}
-	// Note 3
 	std::ostringstream buffer;
 	buffer << input_file.rdbuf();
-	// The same like (but this is more efficient)
-	// std::ostringstream buffer(input_file.rdbuf());
-	// Or the same like
-	// std::ostringstream buffer << input_file.rdbuf();
+
 	input_file.close();
 	return (buffer.str());
 }
@@ -36,7 +30,6 @@ void writeStringIntoFile(const std::string &path, const std::string &content)
 	output_file.close();
 }
 
-// In C we would use the function strstr to find the first occurrence of a substring in a string.
 std::string replaceOccurences(std::string fileContent, const std::string toReplace, const std::string replacer)
 {
 	size_t pos = 0;
@@ -47,16 +40,10 @@ std::string replaceOccurences(std::string fileContent, const std::string toRepla
 		std::string strBefore = fileContent.substr(0, pos);
 		std::string strAfter = fileContent.substr(pos + toReplaceLength);
 		fileContent = strBefore + replacer + strAfter;
-		// The serarch should continue after the replaced string
 		pos += replacerLength;
 	}
 	return (fileContent);
 }
-
-// TODO: think about edge cases
-// - s1 and s2 can't be NULL
-// - s1 and s2 can be empty strings
-// - s1 and s2 can be the same string
 
 void checks(std::string &s1, std::string &s2)
 {
@@ -93,44 +80,3 @@ int main(int argc, char **argv)
 	std::cout << new_content << std::endl;
 	writeStringIntoFile(file_path + ".replace", new_content);
 }
-
-// Note 1
-/*
-The c_str() function is a member function of the std::string class in C++. It
-returns a pointer to an array that contains a null-terminated sequence of
-characters (i.e., a C-string) representing the current value of the
-std::string object.
-
-In the context of your function readFileIntoString, c_str() is called on the
-path variable, which is of type std::string, to convert it to a C-style
-string. Here's why it's used:
-
-In C++98, the constructor of std::ifstream (input file stream) does not accept
-a std::string directly. Instead, it expects a const char* type, which is the
-traditional C-style string. The c_str() function is used to convert the
-std::string path to a C-style string, so it can be passed to the std::ifstream
-constructor.
- */
-
-// Note 2
-// Check if the file has been successfully opened
-// is_open() returns true if the file is open, false otherwise
-
-/* Note 3
-
-std::ostringstream buffer;
-buffer << input_file.rdbuf();
-
-The same like (but this is more efficient)
-std::ostringstream buffer(input_file.rdbuf());
-
-Or the same like
-std::ostringstream buffer << input_file.rdbuf();
-
-std::string buffer;
-std::string line;
-while (std::getline(input_file, line)) {
-	buffer += line + '\n'; // Repeatedly append to the string
-}
-
-*/
