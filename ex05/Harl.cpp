8678fe4536b6897e301293203e8db554ace75a04
552020 <stefanolombardo@posteo.de>
2024-02-23 19:44:56 +0100

    some changes

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -26,6 +26,7 @@ void Harl::warning(void)
 }
 
 void Harl::error(void) { std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl; }
+
 void Harl::complain(std::string level)
 {
 	int nbrOfComplainFunctions = 4;
