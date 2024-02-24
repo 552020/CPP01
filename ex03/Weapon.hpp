fc9070dfe71cdc08a77b8a13c84e5b21405eb525
552020 <stefanolombardo@posteo.de>
2024-02-24 20:22:31 +0100

    small formatting

--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -3,16 +3,16 @@
 
 #include <string>
 
-class Weapon {
-public:
-  Weapon(std::string type);
-  ~Weapon();
-  // std::string const &getType() const;
-  std::string getType() const;
-  void setType(std::string type);
+class Weapon
+{
+  public:
+	Weapon(std::string type);
+	~Weapon();
+	std::string getType() const;
+	void setType(std::string type);
 
-private:
-  std::string _type;
+  private:
+	std::string _type;
 };
 
 #endif
