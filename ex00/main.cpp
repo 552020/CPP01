0e2537008e8207757b1b297d25785a2368e8e9ee
Stefano Lombardo <slombard@4b0c3.42berlin.de>
2024-02-24 20:00:11 +0100

    some minor tweaks

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,23 +6,23 @@ int main()
 	std::cout << "We create a Zombie object on the heap." << std::endl;
 	std::cout << "First we declare a pointer to a Zombie object." << std::endl;
 	std::cout << "Then we call the newZombie() function to create a Zombie object." << std::endl;
-	std::cout << "We could also have done this in two steps." << std::endl;
 	std::string zedName = "Zed";
 	Zombie *zedPtr = newZombie(zedName);
-	std::cout << "The Zombie" << zedName << " has been created." << std::endl;
-	std::cout << "Zed is going to announce itself." << std::endl;
+	std::cout << "The Zombie " << zedName << " has been created." << std::endl;
+	std::cout << "Zed is going to announce itself.\n" << std::endl;
 	zedPtr->announce();
-	std::cout << zedName << " has announced itself." << std::endl;
+	std::cout << "\n" << zedName << " has announced itself." << std::endl;
 	randomChump("Dave");
+	std::cout << "...\n" << "..\n" << ".\n" << std::endl;
 	std::cout << zedName << " outlived Dave." << std::endl;
-	std::cout << "It can now announce itself again." << std::endl;
+	std::cout << "It can now announce itself again.\n" << std::endl;
 	zedPtr->announce();
 	std::cout << zedName << " has announced itself again." << std::endl;
-	std::cout << "But not sadly is going to die." << std::endl;
+	std::cout << "But notw sadly is going to die." << std::endl;
 	// Deleting the dynamically allocated Zombie, will call the destructor
 	delete zedPtr;
 	std::cout << "Zed is dead." << std::endl;
-	std::cout << "If we would not have deleted it, it would have been a memory leak." << std::endl;
+	std::cout << "If we would not have deleted it: leak." << std::endl;
 
 	return 0;
 }
--- ex00/randomChump.cpp
+++ ex00/randomChump.cpp
@@ -6,11 +6,11 @@ void randomChump(std::string name)
 	std::cout << "We enter the scope of randomChump()" << std::endl;
 	std::cout << "We create a Zombie object on the stack." << std::endl;
 	Zombie chumpZombie(name);
-	std::cout << "The Zombie" << name << " has been created." << std::endl;
-	std::cout << "We call the announce() method of the Zombie object." << std::endl;
+	std::cout << "The Zombie " << name << " has been created." << std::endl;
+	std::cout << "We call the announce() method of the Zombie object.\n" << std::endl;
 	chumpZombie.announce();
-	std::cout << "The Zombie" << name << " has announced itself." << std::endl;
+	std::cout << "\nThe Zombie " << name << " has announced itself." << std::endl;
 	std::cout << "We are going to exit the scope of randomChump()" << std::endl;
 	std::cout << "The Zombie" << name << " is going to die with its scope." << std::endl;
-	std::cout << "Farewell, Zombie" << name << "!" << std::endl;
+	std::cout << "Farewell, Zombie " << name << "!" << std::endl;
 }
